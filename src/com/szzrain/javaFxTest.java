ed2b72560805bde502e9fc7e1ef22c70bf55e9e3
szzrain <szzrain@gmail.com>
2020-07-24 19:05:37 +0800

    javaDocs

--- src/com/szzrain/javaFxTest.java
+++ src/com/szzrain/javaFxTest.java
@@ -11,25 +11,44 @@ import javafx.stage.Stage;
 
 import java.util.ArrayList;
 
-
+/**
+ * @author SzzRain
+ * @throws NameException if computer name is illegal, then throws this exception
+ */
 public class javaFxTest extends Application implements EventHandler<MouseEvent> {
 
+    Scene createComputerScene, homeScene;
     public static ArrayList<Laptop> laptops = new ArrayList();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         TextField textField = new TextField();
+
+
         Button button = new Button("输出字符");
         button.setOnMouseClicked(event -> System.out.println(textField.getText()));
 
+//        TODO:
+//        Button
+
+
         VBox vBox = new VBox();
         vBox.getChildren().addAll(button, textField);
+
+
         Scene scene = new Scene(vBox, 400, 400);
+
+
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    static void getComputer(String name, String brand, String colour) {
+    /**
+     * @param brand  品牌
+     * @param colour 颜色
+     * @param name   名字
+     */
+    static void createComputer(String name, String brand, String colour) {
         try {
             laptops.add(new Laptop(name, brand, colour));
             ButtonFrame.computerNumber++;
@@ -38,6 +57,18 @@ public class javaFxTest extends Application implements EventHandler<MouseEvent> 
             System.out.println("can not be like that!");
         }
     }
+
+    /**
+     * @param num 对象索引值
+     */
+    static void getComputer(int num) {
+        try {
+            System.out.println(laptops.get(num));
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("That's not work!");
+        }
+    }
+
 
     public static void main(String[] args) {
         System.out.println("Window start");
